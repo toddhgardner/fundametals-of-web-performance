548ec92bd63cbb7bef8a9c93356b3e198f9c97d0
Todd Gardner <todd@g12solutions.com>
2024-09-19 10:53:57 -0500

    initial express5 install with simple static hosting

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+
+const express = require("express");
+const { resolve } = require("path");
+
+const app = express();
+
+/**
+ * Host static assets in the ./public directory
+ */
+app.use(express.static(resolve(__dirname, "public")));
+
+/**
+ * Start the application
+ */
+app.listen(3000, () => {
+  console.log("Application is ready at http://localhost:3000/");
+});
