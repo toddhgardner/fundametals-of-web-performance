9a70f7ce97ce9400a1482cfa46e49e6e2dc2c2d0
Todd Gardner <todd@g12solutions.com>
2024-09-21 15:05:54 -0500

    more delay

--- server/performance-config.js
+++ server/performance-config.js
@@ -24,7 +24,7 @@ module.exports = {
    * The expected processing time in milliseconds of a "real" server under load
    * that has to talk to external systems.
    */
-  serverDuration: 100,
+  serverDuration: 400,
 
   /**
    * The network latency in milliseconds to simulate on requests. This is based
