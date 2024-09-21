30135ce51fd6e7c676f14fcdff2b47f74d26cfef
Todd Gardner <todd@g12solutions.com>
2024-09-21 15:10:16 -0500

    proper big delay

--- server/performance-config.js
+++ server/performance-config.js
@@ -24,7 +24,7 @@ module.exports = {
    * The expected processing time in milliseconds of a "real" server under load
    * that has to talk to external systems.
    */
-  serverDuration: 400,
+  serverDuration: 1_000,
 
   /**
    * The network latency in milliseconds to simulate on requests. This is based
