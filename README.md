46ec1e356928ca9d6cf566dfd61a158b14a6d038
Todd Gardner <todd@g12solutions.com>
2024-09-25 17:48:24 -0500

    samples and readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Fundamentals of Web Performance Workshop
 
-Example website teaching web performance concepts.
+Example Online store **Developer Stickers Online** for the Fundamentals of Web Performance Workshop.
+
+## Getting Started
+
+This is a webserver built on NodeJS and Express that servers plain HTML, CSS, JavaScript and Images. You need to have **Node ~20.0** to run this project.
+
+1. Install Dependencies `npm install`
+2. Launch the Dev Server `npm start`
+3. Open the website at http://localhost:3000/
+
+### Instructor Setup
+
+To do deploys, you need to create the `.env` file from the `.env.template`. You'll also need to enable execute on the script, `chmod +x ./tools/deploy.sh`
+
+## Workshop Info
+In this one-day training, you'll learn how to make your website *fast*, and keep it that way.You'll learn how to set performance goals for your project, how to measure them, and improve your website performance.
+ 
+### Benefits
+- Know why website performance is important for your project and business success on the web.
+- Understand the Core Web Vitals and other performance metrics, and the behavior being measured.
+- Linking the business goals of your website to performance metrics.
+- Technology and tactics to improve your metrics and the user experience of your website.
+
+### Summary
+Build faster websites and web applications by learning the current metrics and techniques for improving web performance. We’ll look at the psychology of web performance and how users see wait time on your site. Then learn about the new Core Web Vitals that Google uses to measure your page, like Largest Contentful Paint (LCP), Cumulative Layout Shift (CLS), and Interation to Next Paint (INP). Finally, we’ll discuss building a performance culture in your organization to start your applications fast from the beginning!
--- public/assets/js/code_samples.js
+++ public/assets/js/code_samples.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
 });
 
 
+
 <html>
   <body>
     <div id="app"></div>
@@ -38,7 +39,87 @@ $(document).ready(function () {
   </body>
 </html>
 
+console.table(
+  [...document.images].map((img) => {
+    const entry = performance.getEntriesByName(img.currentSrc)[0];
+    const bytes = (entry?.encodedBodySize * 8);
+    const pixels = (img.width * img.height);
+    return { src: img.currentSrc, bytes, pixels, entropy: (bytes / pixels) };
+  })
+)
 
+
+
+
+function task1() {
+  task2();
+}
+
+function task2() {
+  task3();
+}
+
+function task3() {
+  // something
+}
+
+
+Date.now()
+//> 1727181644813
+
+performance.now()
+//> 8994.199999988079
+
+performance.timeOrigin
+//> 1727181678939.8
+
+performance.timeOrigin + performance.now()
+//> 1727181763103.9001
+
+
+
+const performanceObserver = new PerformanceObserver((list, observer) => {
+  list.getEntries().forEach((entry) => {
+    console.log(`Layout shifted by ${entry.value}`);
+  })
+});
+performanceObserver.observe({ type: "layout-shift", buffered: true });
+
+
+import { onLCP, onCLS, onINP } from "./web-vitals.mjs";
+
+onLCP(console.log);
+onCLS(console.log);
+onINP(console.log);
+
+
+import { RM } from "@request-metrics/browser-agent";
+
+RM.install({
+  token: "your-app-token",
+  /* other settings */
+})
+
+const el = document.createElement("script")
+el.setAttribute("src", "/otherScript.js");
+document.body.appendChild(el);
+
+
+
+<picture class="illustration">
+  <source media="(max-width: 720px)"
+    srcset="/hero-mobile.png?width=360 360w,
+            /hero-mobile.png?width=720 720w,
+            /hero-mobile.png?width=1440 1440w" />
+  <source media="(min-width: 721px)"
+    srcset="/hero-desktop.png?width=720 720w,
+            /hero-desktop.png?width=1440 1440w,
+            /hero-desktop.png?width=2800 2800w">
+    <img src="/hero-desktop.png?width=2800"
+      alt="Developer Stickers Online"
+      fetchpriorty="high"
+      height="1200" width="2800" />
+</picture>
 
 
 
