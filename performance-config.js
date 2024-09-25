8e188d388f86b4bc34fda6c9eedd1599eb2ba8c2
Todd Gardner <todd@g12solutions.com>
2024-09-25 10:27:05 -0500

    even longer

--- performance-config.js
+++ performance-config.js
@@ -34,6 +34,6 @@ module.exports = {
    * The expected processing time in milliseconds of a "real" server under load
    * that has to talk to external systems.
    */
-  serverDuration: 500
+  serverDuration: 900
 
 };
